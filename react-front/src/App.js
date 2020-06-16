4fb606083260397fcbb9037062881d47356d79e9
rajan singh <rajan9519@gmail.com>
2020-06-16 07:33:16 +0530

    basic navigation bar

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { Home, SignUp, Profile, SignIn } from "./components/screens";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/SignUp">
+        <SignUp />
+      </Route>
+      <Route exact path="/SignIn">
+        <SignIn />
+      </Route>
+      <Route exact path="/Profile">
+        <Profile />
+      </Route>
+    </BrowserRouter>
   );
 }
 
---
+++ react-front/src/components/screens/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Profile from "./Profile";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+export { Home, Profile, SignUp, SignIn };
