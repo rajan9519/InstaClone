ebd2db72285a9a3fe42d841120dd1565d46519f1
rajan singh <rajan9519@gmail.com>
2020-06-19 11:19:40 +0530

    getting image and showing it

--- Server/routes/post.js
+++ Server/routes/post.js
@@ -8,6 +8,14 @@ const GridFsStorage = require("multer-gridfs-storage");
 const Grid = require("gridfs-stream");
 const mongoURI = require("../keys").MONGO_URL;
 
+const conn = mongoose.createConnection(mongoURI);
+
+let gfs;
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("images");
+});
+
 // Create storage engine
 const storage = new GridFsStorage({
   url: mongoURI,
@@ -31,14 +39,60 @@ const upload = multer({ storage });
 
 const Post = mongoose.model("Post");
 router.get("/", (req, res) => {
-  Post.find()
-    .populate("postedBy", "_id name")
-    .then((posts) => {
-      res.json({ posts });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // Post.find()
+  //   .populate("postedBy", "_id name")
+  //   .then((posts) => {
+  //     res.json({ posts });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      res.render("index", { files: false });
+    } else {
+      files.map((file) => {
+        if (
+          file.contentType === "image/jpeg" ||
+          file.contentType === "image/png"
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res.json(files);
+    }
+  });
+});
+
+router.get("/test/:filename", (req, res) => {
+  res.send(
+    `<img src="/post/image/${req.params.filename}" width="100%" height="100%" alt=""></img>`
+  );
+});
+router.get("/image/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exists",
+      });
+    }
+
+    // Check if image
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image",
+      });
+    }
+  });
 });
 
 router.post("/createPost", upload.single("file"), (req, res) => {
