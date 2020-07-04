149b3875292b1d7aca267736fa86804b7f51fa03
rajan singh <rajan9519@gmail.com>
2020-07-04 15:38:05 +0530

    sending private messages who are online

--- Server/chatServer.js
+++ Server/chatServer.js
@@ -9,9 +9,19 @@ const io = socketio(server);
 
 app.use(cors());
 
+const user = {};
+
 io.on("connect", (socket) => {
-  socket.on("join", (name) => {
-    console.log(`${name} is online now`);
+  socket.on("join", (username) => {
+    //socket.join();
+    console.log(`${username} is online now`);
+    user[username] = socket.id;
+  });
+  socket.on("send", (data) => {
+    io.to(user[data.to]).emit("message", {
+      message: data.message,
+      from: data.from,
+    });
   });
   socket.on("disconnect", () => {
     console.log(`user left ${socket.id}`);
--- react-front/src/components/presentation/ChatRoom.js
+++ react-front/src/components/presentation/ChatRoom.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 let socket;
@@ -6,11 +6,69 @@ let socket;
 const ENDPOINT = "http://localhost:8000";
 
 const ChatRoom = () => {
+  const [message, setMessage] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+
   useEffect(() => {
     socket = io(ENDPOINT);
-    socket.emit("join", "rajan");
   }, [ENDPOINT]);
-  return <h1>You are in ChatRoom</h1>;
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      console.log(data);
+    });
+  }, []);
+  const send = (e) => {
+    e.preventDefault();
+    socket.emit("send", {
+      to,
+      from,
+      message,
+    });
+  };
+  const join = (e) => {
+    e.preventDefault();
+    socket.emit("join", from);
+  };
+  return (
+    <div>
+      <h1>You are in ChatRoom</h1>
+      <input
+        placeholder="send from"
+        value={from}
+        onChange={(e) => setFrom(e.target.value)}
+        required
+      />
+      <button
+        onClick={(e) => {
+          join(e);
+        }}
+      >
+        Join
+      </button>
+      <input
+        placeholder="send something"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        required
+      />
+      <input
+        placeholder="send to"
+        value={to}
+        onChange={(e) => setTo(e.target.value)}
+        required
+      />
+      <button
+        type="submit"
+        onClick={(e) => {
+          send(e);
+        }}
+      >
+        Send
+      </button>
+    </div>
+  );
 };
 
 export default ChatRoom;
